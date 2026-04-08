aceb5884ecf3b0750f3df8b3c0513229748dd0fa
ryochin33 <1210.ryo.i@gmail.com>
2026-04-09 05:37:55 +0900

    fix: Viteのデフォルトポート(5173)を許可リストに追加

--- backend/memo.py
+++ backend/memo.py
@@ -7,7 +7,10 @@ app = FastAPI()
 # Reactからの通信を許可する設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173" #http://localhost:5173 を追加
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
